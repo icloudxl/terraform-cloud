c5ea934ce08cdb9bbbd42e56f9a44a20c2223e73
icloudxl <94758188+icloudxl@users.noreply.github.com>
2021-11-20 10:20:59 -0800

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+	region = "us-east-1"
+}
+
+
+resource "aws_instance" "hello-world" {
+
+ ami = "ami-0a313d6098716f372" 
+ instance_type = "t2.micro"
+ key_name = "terraform"
+ tags = {
+	 Name = "Hello-world"
+ }
+
+}
